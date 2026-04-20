9cd642d0d555cd88f52aa668e9029d8508c3a5e7
Denis Jamil <denistineohuancas@gmail.com>
2026-04-20 14:04:42 -0500

    Estructura del backend

---
+++ olimpiadas-peru/src/main/java/com/sistema/olimpiadas_peru/OlimpiadasPeruApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.olimpiadas_peru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlimpiadasPeruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlimpiadasPeruApplication.class, args);
+	}
+
+}
---
+++ olimpiadas-peru/src/test/java/com/sistema/olimpiadas_peru/OlimpiadasPeruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.olimpiadas_peru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlimpiadasPeruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
