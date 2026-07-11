397f05b44172548730df0d5bdb10dfca46b695bd
Denis Jamil <denistineohuancas@gmail.com>
2026-07-10 23:57:28 -0500

    fix : correcion de vista del modulo de paises

--- frontend/src/app/paises/page.tsx
+++ frontend/src/app/paises/page.tsx
@@ -1,10 +1,13 @@
+import { AppShell } from "@/components/layout/AppShell";
 import { PaisesClient } from "@/app/paises/PaisesClient";
 import { RequireModule } from "@/components/auth/RequireModule";
 
 export default function PaisesPage() {
   return (
-    <RequireModule keys={["/instituciones", "instituciones"]}>
-      <PaisesClient />
-    </RequireModule>
+    <AppShell>
+      <RequireModule keys={["/instituciones", "instituciones"]}>
+        <PaisesClient />
+      </RequireModule>
+    </AppShell>
   );
 }
